a18f5261e9467e4f8935b4cbf48729bd54e3d8cb
Jhonny Mendoza <jhomen11@gmail.com>
2022-01-22 22:21:42 -0300

    datos input formularios

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,33 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Formulario = () => {
+  const [mascota, setMascota] = useState("");
+  const [propietario, setPropietario] = useState("");
+  const [email, setEmail] = useState("");
+  const [fecha, setFecha] = useState("");
+  const [sintomas, setSintomas] = useState("");
+
   return (
     <div className="col-12 col-md-5  my-3 ">
-      <h3 className="text-center fw-bold mb-4">Añadir <span className="text-primary">Pacientes</span></h3>
+      <h3 className="text-center fw-bold mb-4">
+        Añadir <span className="text-primary">Pacientes</span>
+      </h3>
       <form action="" className="my-4 bg-white shadow-sm rounded p-4">
-          <div className="mb-3">
-              <label htmlFor="mascota" className="form-label fw-bold">Nombre Mascota</label>
-              <input type="text" className="form-control" id="mascota" placeholder="Nombre de la Mascota"/>
-          </div>
-          <div className="mb-3">
-              <label htmlFor="propietario" className="form-label fw-bold">Nombre Propietario</label>
-              <input type="text" className="form-control" id="propietario" placeholder="Nombre del Propietario"/>
-          </div>
-          <div className="mb-3">
-              <label htmlFor="email" className="form-label fw-bold">Email</label>
-              <input type="email" className="form-control" id="email" placeholder="correo@correo.com"/>
-          </div>
-          <div className="mb-3">
-              <label htmlFor="alta" className="form-label fw-bold">Alta</label>
-              <input type="date" className="form-control" id="alta"/>
-          </div>
-          <div className="mb-3">
-              <label htmlFor="sintomas" className="form-label fw-bold">Sintomas</label>
-              <textarea type="sintomas" className="form-control" id="sintomas" placeholder="Describe los sintomas"/>
-          </div>
-          <div className=" d-grid ">
-          <input type="submit" className="btn btn-primary" value="Agregar Paciente" />
-          </div>
+        <div className="mb-3">
+          <label htmlFor="mascota" className="form-label fw-bold">
+            Nombre Mascota
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="mascota"
+            placeholder="Nombre de la Mascota"
+            value={mascota}
+            onChange={(e) => setMascota(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="propietario" className="form-label fw-bold">
+            Nombre Propietario
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="propietario"
+            placeholder="Nombre del Propietario"
+            value={propietario}
+            onChange={(e) => setPropietario(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label fw-bold">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            placeholder="correo@correo.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="alta" className="form-label fw-bold">
+            Alta
+          </label>
+          <input type="date" 
+            className="form-control" id="alta"
+            value={fecha}
+            onChange={ (e) => setFecha(e.target.value) }  
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="sintomas" className="form-label fw-bold">
+            Sintomas
+          </label>
+          <textarea
+            type="sintomas"
+            className="form-control"
+            id="sintomas"
+            placeholder="Describe los sintomas"
+            value={sintomas}
+            onChange={ (e) => setSintomas(e.target.value) }
+          />
+        </div>
+        <div className=" d-grid ">
+          <input
+            type="submit"
+            className="btn btn-primary"
+            value="Agregar Paciente"
+          />
+        </div>
       </form>
     </div>
   );
