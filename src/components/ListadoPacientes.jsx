2c09254f3cdf3f0e8fc684c224fb91f707aac739
Jhonny Mendoza <jhomen11@gmail.com>
2022-01-25 09:22:07 -0300

    Renderizado condicional listado pacientes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,18 +1,24 @@
-import Paciente from "./Paciente"
-
+import Paciente from "./Paciente";
 
 const ListadoPacientes = ({ pacientes }) => {
-    return (
-        <div className="col-12 col-md-6 my-3">
-            <h3 className="text-center fw-bold">Listado <span className="text-primary">Pacientes</span></h3>
-            
-            {pacientes.map((el)=>{
-                return(
-                    <Paciente key={el.id} el={el}/>
-                )
-            })}
-        </div>
-    )
-}
+  return (
+    <div className="col-12 col-md-6 my-3">
+      {pacientes && pacientes.length ? (
+        <>
+          <h3 className="text-center fw-bold">
+            Listado <span className="text-primary">Pacientes</span>
+          </h3>
 
-export default ListadoPacientes
+          {pacientes.map((el) => {
+            return <Paciente key={el.id} el={el} />;
+          })}
+        </>
+      ) : (
+        <h3 className="text-center fw-bold">
+          No hay <span className="text-primary">Citas</span>
+        </h3>
+      )}
+    </div>
+  );
+};
+export default ListadoPacientes;
