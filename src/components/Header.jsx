1a96d3dcf24c83218870250c86c43d2e46f536eb
Jhonny <jhomen11@gmail.com>
2022-02-01 16:58:57 -0300

    Modificacion titulo2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <>
       <h1 className="text-center my-3 fw-bold">
-        Control de  Citas Pacientes <span className="text-primary">Veterinaria</span>
+        Control de  Citas <span className="text-primary">Veterinaria</span>
       </h1>
     </>
   );
